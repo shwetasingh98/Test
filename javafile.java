ecfba5d98c8905fd94f3fca921af0d5a62cab768
mohammadshahnawaj <108412676+mohammadshahnawaj@users.noreply.github.com>
2022-07-07 15:34:02 +0530

    Update javafile.java

--- javafile.java
+++ javafile.java
@@ -1,1 +1,9 @@
 My first javascript file.
+  
+select * from customers c right join orders o on c.customer_id= o.customer_id;
+
+select * from customers c left join orders o on c.customer_id= o.customer_id union all
+select * from customers c right join orders o on c.customer_id= o.customer_id;
+select * from customer c cross join orders o on c.customer_id= o.customer_id;
+
+select * from customers c inner join orders o on c.customer_id= o.customer_id;
